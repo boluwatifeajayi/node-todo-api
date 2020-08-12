1065fe59a3149c1c90d1fd3da0de00bda689fb4a
Boluwatife Ajayi <bolu4good@gmail.com>
2020-08-12 18:07:50 +0100

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+//destructuring
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+//destructuring
+// var user = {name: 'bolu', age: 17};
+// var {name} = user;
+
+// console.log(name);
+
+ MongoClient.connect('mongodb://localhost:27017/TodoApp', {useUnifiedTopology: true, useNewUrlParser:true}, (err, client) =>{
+    if (err) {
+       return console.log('Unable to connect to mongo db server');
+    }
+    console.log('connected to mongo db server');
+
+    //insert into database
+    //const db = client.db('TodoApp')
+
+    //  db.collection('Todo').insertOne({
+        
+    //     text: 'Something To Do',
+    //     completed: false
+    // }, (err, result) =>{
+    //     if (err) {
+    //         return console.log('Unable to insert Todo');
+    //     }
+
+    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+    //     client.close();
+    // });
+
+   client.close();
+
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,35 @@
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
+
+ MongoClient.connect('mongodb://localhost:27017/TodoApp', {
+             useUnifiedTopology: true,
+             useNewUrlParser: true
+         }, (err, client) => {
+             if (err) {
+                 return console.log('Unable to connect to mongo db server');
+             }
+             console.log('connected to mongo db server');
+             const db = client.db('TodoApp')
+            //fetch todos
+            //  db.collection('Todos').find(/*{completed: false}*/).toArray().then((docs) => {
+            //     console.log('Todos');
+            //     console.log(JSON.stringify(docs, undefined, 2));
+            //  }, (err) => {
+            //      console.log("unable to fetch todos", err);
+            //      //client.close();
+            //  });
+
+            //count todos
+            db.collection('Todos').find( /*{completed: false}*/ ).count().then((count) => {
+                console.log(`Todos counts: ${count}`);
+                //console.log(JSON.stringify(docs, undefined, 2));
+            }, (err) => {
+                console.log("unable to fetch todos", err);
+                //client.close();
+            });
+
+             
+
+            });
